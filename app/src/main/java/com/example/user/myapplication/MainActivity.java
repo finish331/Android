5cb6a65b8340470fa0e9a7241d8a39c934c9cc73
a105590030 <t105590030@ntut.edu.tw>
2018-04-01 23:16:13 +0800

    HW4

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.myapplication;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -12,95 +14,119 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Spinner mSpnSex;
     private RadioGroup mRadGrp;
-    private RadioButton mRadBtnAgeRange1;
-    private RadioButton mRadBtnAgeRange2;
-    private RadioButton mRadBtnAgeRange3;
-    private TextView mTxtNumFamily;
-    private NumberPicker mNumPkrFamily;
+    private RadioButton mRadBtnMale;
+    private RadioButton mRadBtnfemale;
+    private Spinner mSpnAge;
+    private CheckBox mInterest1;
+    private CheckBox mInterest2;
+    private CheckBox mInterest3;
+    private CheckBox mInterest4;
+    private CheckBox mInterest5;
+    private CheckBox mInterest6;
+    private CheckBox mInterest7;
+    private CheckBox mInterest8;
+    private CheckBox mInterest9;
+    private CheckBox mInterest10;
     private Button mBtnOK;
     private TextView mTxtSug;
-
-    private String selectedSex;
+    private TextView mTxtInterset;
+    private String selectedAge;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSpnSex = (Spinner) findViewById(R.id.spnSex);
-        mRadGrp = (RadioGroup) findViewById(R.id.radGrpAge);
-        mRadBtnAgeRange1 = (RadioButton) findViewById(R.id.radBtnAgeRange1);
-        mRadBtnAgeRange2 = (RadioButton) findViewById(R.id.radBtnAgeRange2);
-        mRadBtnAgeRange3 = (RadioButton) findViewById(R.id.radBtnAgeRange3);
-        mTxtNumFamily = (TextView) findViewById(R.id.txtNumFamily);
-        mNumPkrFamily = (NumberPicker) findViewById(R.id.numPkrFamply);
-        mNumPkrFamily.setMinValue(0);
-        mNumPkrFamily.setMaxValue(20);
-        mNumPkrFamily.setValue(3);
+        mRadGrp =(RadioGroup) findViewById(R.id.radGrpSex);
+        mRadBtnMale = (RadioButton) findViewById(R.id.radBtnSex1);
+        mRadBtnfemale = (RadioButton) findViewById(R.id.radBtnSex2);
+        mSpnAge = (Spinner) findViewById(R.id.spnAge);
+        mInterest1 = (CheckBox) findViewById(R.id.interest1);
+        mInterest2 = (CheckBox) findViewById(R.id.interest2);
+        mInterest3 = (CheckBox) findViewById(R.id.interest3);
+        mInterest4 = (CheckBox) findViewById(R.id.interest4);
+        mInterest5 = (CheckBox) findViewById(R.id.interest5);
+        mInterest6 = (CheckBox) findViewById(R.id.interest6);
+        mInterest7 = (CheckBox) findViewById(R.id.interest7);
+        mInterest8 = (CheckBox) findViewById(R.id.interest8);
+        mInterest9 = (CheckBox) findViewById(R.id.interest9);
+        mInterest10 = (CheckBox) findViewById(R.id.interest10);
         mBtnOK = (Button) findViewById(R.id.btnOK);
         mTxtSug = (TextView) findViewById(R.id.txtSug);
-
-        mSpnSex.setOnItemSelectedListener(spnOnItemSelect);
-        mNumPkrFamily.setOnValueChangedListener(numPkrFamilyOnValueChange);
+        mTxtInterset = (TextView) findViewById(R.id.txtInterest);
         mBtnOK.setOnClickListener(btnOKOnClick);
     }
-
-    private AdapterView.OnItemSelectedListener spnOnItemSelect = new AdapterView.OnItemSelectedListener() {
+    /*private AdapterView.OnItemSelectedListener spnOnItemSelect = new AdapterView.OnItemSelectedListener(){
         @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            selectedSex = parent.getSelectedItem().toString();
-
-            switch (selectedSex) {
-                case "male":
-                    mRadBtnAgeRange1.setText(getString(R.string.maleAgeRange1));
-                    mRadBtnAgeRange2.setText(getString(R.string.maleAgeRange2));
-                    mRadBtnAgeRange3.setText(getString(R.string.maleAgeRange3));
-                    break;
-                case "female":
-                    mRadBtnAgeRange1.setText(getString(R.string.femaleAgeRange1));
-                    mRadBtnAgeRange2.setText(getString(R.string.femaleAgeRange2));
-                    mRadBtnAgeRange3.setText(getString(R.string.femaleAgeRange3));
-            }
+        public  void onItemSelected(AdapterView<?> parent, View view , int position, long id){
+            selectedAge = parent.getSelectedItem().toString();
         }
-
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
 
         }
-    };
-
-    private NumberPicker.OnValueChangeListener numPkrFamilyOnValueChange = new NumberPicker.OnValueChangeListener() {
+    };-*/
+    private View.OnClickListener btnOKOnClick = new View.OnClickListener(){
         @Override
-        public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-            mTxtNumFamily.setText(String.valueOf(newVal));
-        }
-    };
-
-    private View.OnClickListener btnOKOnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            MarriageSuggestion ms = new MarriageSuggestion();
-
-            String strSex = mSpnSex.getSelectedItem().toString();
-            int iAgeRange = 0;
-
+        public  void onClick(View v){
+            String strAge = mSpnAge.getSelectedItem().toString();
+            String s = "您的興趣：";
             switch (mRadGrp.getCheckedRadioButtonId()) {
-                case R.id.radBtnAgeRange1:
-                    iAgeRange = 1;
+                case R.id.radBtnSex1:
+                    if(strAge.equals("(男)小於30歲 / (女)小於28歲")){
+                        mTxtSug.setText("婚姻建議：還不急");
+                    }
+                    if(strAge.equals("(男)30~40歲 / (女)28歲~35歲")){
+                        mTxtSug.setText("婚姻建議：趕快結婚");
+                    }
+                    if(strAge.equals("(男)40歲以上 / (女)大於35歲")){
+                        mTxtSug.setText("婚姻建議：開始找對象");
+                    }
                     break;
-                case R.id.radBtnAgeRange2:
-                    iAgeRange = 2;
-                    break;
-                case R.id.radBtnAgeRange3:
-                    iAgeRange = 3;
+                case R.id.radBtnSex2:
+                    if(strAge.equals("(男)小於30歲 / (女)小於28歲")){
+                        mTxtSug.setText("婚姻建議：還不急");
+                    }
+                    if(strAge.equals("(男)30~40歲 / (女)28歲~35歲")){
+                        mTxtSug.setText("婚姻建議：趕快結婚");
+                    }
+                    if(strAge.equals("(男)40歲以上 / (女)大於35歲")){
+                        mTxtSug.setText("婚姻建議：開始找對象");
+                    }
                     break;
             }
+            if(mInterest1.isChecked()){
+                s += mInterest1.getText().toString();
+            }
+            if(mInterest2.isChecked()){
+                s += mInterest2.getText().toString();
+            }
+            if(mInterest3.isChecked()){
+                s += mInterest3.getText().toString();
+            }if(mInterest4.isChecked()){
+                s += mInterest4.getText().toString();
+            }
+            if(mInterest5.isChecked()){
+                s += mInterest5.getText().toString();
+            }
+            if(mInterest6.isChecked()){
+                s += mInterest6.getText().toString();
+            }if(mInterest7.isChecked()){
+                s += mInterest7.getText().toString();
+            }
+            if(mInterest8.isChecked()){
+                s += mInterest8.getText().toString();
+            }
+            if(mInterest9.isChecked()){
+                s += mInterest9.getText().toString();
+            }
+            if(mInterest10.isChecked()){
+                s += mInterest10.getText().toString();
+            }
+            mTxtInterset.setText(s);
 
-            mTxtSug.setText(ms.getSuggestion(strSex, iAgeRange, mNumPkrFamily.getValue()));
         }
     };
+
 }
