f85dba17163e16d2fe574d8358f6b1afa5ac3a90
a105590030 <t105590030@ntut.edu.tw>
2018-05-23 23:36:33 +0800

    Hw9

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.user.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -12,6 +18,7 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,6 +26,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MENU_MUSIC = Menu.FIRST,
+                            MENU_PLAY_MUSIC = Menu.FIRST + 1,
+                            MENU_STOP_PLAYING_MUSIC = Menu.FIRST + 2,
+                            MENU_ABOUT = Menu.FIRST + 3,
+                            MENU_EXIT = Menu.FIRST + 4;
+    private RelativeLayout mRelativeLayout;
+
     private Spinner itemSpinner;
     private EditText edtDate,edtMoney;
     private DatePicker chooseDate;
@@ -35,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
         if(bundle!=null){
             result = bundle.getStringArrayList("result");
         }
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relative);
+        registerForContextMenu(mRelativeLayout);
         itemSpinner = (Spinner) findViewById(R.id.itemSpinner);
         edtDate = (EditText) findViewById(R.id.edtDate);
         edtMoney = (EditText) findViewById(R.id.edtMoney);
@@ -61,6 +77,60 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        SubMenu subMenu = menu.addSubMenu(0,MENU_MUSIC,0,"背景音樂")
+                .setIcon(android.R.drawable.ic_media_ff);
+        subMenu.add(0,MENU_PLAY_MUSIC,0,"播放背景音樂");
+        subMenu.add(0,MENU_STOP_PLAYING_MUSIC,1,"停止播放背景音樂");
+        menu.add(0,MENU_ABOUT,1,"關於這個程式...").setIcon(android.R.drawable.ic_dialog_info);
+        menu.add(0,MENU_EXIT,2,"結束").setIcon(android.R.drawable.ic_menu_close_clear_cancel);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case MENU_ABOUT:
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("關於這個程式")
+                        .setMessage("選單範例程式")
+                        .setCancelable(false)
+                        .setIcon(android.R.drawable.star_big_on)
+                        .setPositiveButton("確定",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                })
+                        .show();
+                return  true;
+            case MENU_EXIT:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu,View v,ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+        if(v == mRelativeLayout){
+            if(menu.size() == 0){
+                SubMenu subMenu = menu.addSubMenu(0,MENU_MUSIC,0,"背景音樂");
+                subMenu.add(0,MENU_PLAY_MUSIC,0,"播放背景音樂");
+                subMenu.add(0,MENU_STOP_PLAYING_MUSIC,1,"停止播放音樂");
+                menu.add(0,MENU_ABOUT,1,"關於這個程式...");
+                menu.add(0,MENU_EXIT,2,"結束");
+            }
+        }
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        onOptionsItemSelected(item);
+        return super.onContextItemSelected(item);
+    }
+
 
     private void resizePikcer(FrameLayout tp){
         List<NumberPicker> npList = findNumberPicker(tp);
